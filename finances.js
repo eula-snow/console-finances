b2e2fbdb6daab4a04d2e02e5f13695d944c9a03f
Maria Konova <arim.novak@gmail.com>
2022-11-16 14:23:53 +0000

    fixes counter variable declaration

--- finances.js
+++ finances.js
@@ -4,24 +4,24 @@ console.log(
 );
 
 var total = 0;
-for (i = 0; i < finances.length; i++) {
+for (var i = 0; i < finances.length; i++) {
   total += finances[i][1];
 }
 console.log("Total: $" + total);
 
 var profits = [];
-for (i = 0; i < finances.length; i++) {
+for (var i = 0; i < finances.length; i++) {
   profits.push(finances[i][1]);
 }
 
 var changes = [];
-for (j = 0; j < profits.length - 1; j++) {
+for (var j = 0; j < profits.length - 1; j++) {
   var change = profits[j + 1] - profits[j];
   changes.push(change);
 }
 
 var totalChanges = 0;
-for (n = 0; n < changes.length; n++) {
+for (var n = 0; n < changes.length; n++) {
   totalChanges += changes[n];
 }
 var averChange = totalChanges / (finances.length - 1);
@@ -30,7 +30,7 @@ console.log("Average Change: $" + averChange.toFixed(2));
 
 var maxChange = changes[0];
 var maxChangeInd = 0;
-for (i = 0; i < changes.length; i++) {
+for (var i = 0; i < changes.length; i++) {
   if (maxChange < changes[i]) {
     maxChange = changes[i];
     maxChangeInd = i;
@@ -45,7 +45,7 @@ console.log(
 
 var minChange = changes[0];
 var minChangeInd = 0;
-for (i = 0; i < changes.length; i++) {
+for (var i = 0; i < changes.length; i++) {
   if (minChange > changes[i]) {
     minChange = changes[i];
     minChangeInd = i;
